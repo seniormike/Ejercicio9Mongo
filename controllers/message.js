37b1c560116019cb7335ddfacb89e03cebeaccc3
seniormike <ma.puentes@uniandes.edu.co>
2020-09-30 01:36:28 -0500

    Updated 95%

--- controllers/message.js
+++ controllers/message.js
@@ -1,18 +1,16 @@
-
 /**
  * Imports
  */
 const fs = require("fs");
 const WebSocket = require("ws");
 const ws = new WebSocket("ws://localhost:3000");
-const {messageValidation} = require("../models/message");
+const { messageValidation } = require("../models/message");
+const conn = require("../lib/MongoUtils");
 
 /**
  * Messages array
  */
-let messages = [].concat(
-  JSON.parse(fs.readFileSync("./messagesArray.json"))
-);
+let messages = [].concat(JSON.parse(fs.readFileSync("./messagesArray.json")));
 
 /**
  * Method that generates a message body based on the parameters.
@@ -35,7 +33,7 @@ const save = () => {
 
 /**
  * Method that add a message to the array.
- * @param {message} message 
+ * @param {message} message
  */
 const pushMessage = (message) => {
   messages.push(makeMessage(message));
@@ -48,85 +46,135 @@ exports.wsCreateMessage = pushMessage;
 
 /**
  * Method that gets all messages in the database.
- * @param {req} req 
- * @param {res} res 
- * @param {next} next 
+ * @param {req} req
+ * @param {res} res
+ * @param {next} next
  */
 exports.getAllMessages = (req, res, next) => {
-  res.status(200).send(messages);
+  conn.then((client) => {
+    client
+      .db("messages")
+      .collection("message")
+      .find({})
+      .toArray((err, data) => {
+        res.status(200).send(data);
+        console.log(data);
+      });
+  }).catch((err)=>{
+    res.status(400).send(err);
+  });
+  //res.status(200).send(messages);
 };
 
 /**
  * Method that validates, creates and saves a message with the given information.
- * @param {req} req 
- * @param {res} res 
- * @param {next} next 
+ * @param {req} req
+ * @param {res} res
+ * @param {next} next
  */
 exports.createMessage = (req, res, next) => {
   const { error } = messageValidation.validate(req.body);
-  if (error){
+  if (error) {
     return res.status(400).send(error);
   }
   pushMessage(req.body);
-  res.status(200).send(messages);
+
+  conn.then((client) => {
+    const newMessage = makeMessage(req.body);
+    client
+      .db("messages")
+      .collection("message")
+      .insertOne(newMessage)
+      .then((data) => {
+        res.status(200).send(newMessage);
+        console.log(data);
+      });
+  }).catch((err)=>{
+    res.status(400).send(err);
+  });
+
+  //res.status(200).send(messages);
   //Update
   ws.send("");
 };
 
 /**
  * Method that gets a message with the given id or timestamp.
- * @param {req} req 
- * @param {res} res 
- * @param {next} next 
+ * @param {req} req
+ * @param {res} res
+ * @param {next} next
  */
 exports.getMessageById = (req, res, next) => {
-  let message = messages.find((item) => item.ts === parseInt(req.params.ts));
-  if(!message){
-    return res.status(400).send("The message with the given id was not found.");
-  }
-  res.status(200).send(message);
+  const id = parseInt(req.params.ts);
+  conn.then((client) => {
+    client
+      .db("messages")
+      .collection("message")
+      .findOne({ts:id})
+      .then((data) => {
+        res.status(200).send(data);
+        console.log(data);
+      });
+  }).catch((err)=>{
+    res.status(400).send(err);
+  });
 };
 
 /**
  * Method that deletes a message with the given id or timestamp.
- * @param {req} req 
- * @param {res} res 
- * @param {next} next 
+ * @param {req} req
+ * @param {res} res
+ * @param {next} next
  */
 exports.deleteMessage = (req, res, next) => {
-  let i = messages.findIndex(
-    (item) => item.ts === parseInt(req.params.ts)
-  );
-  if (i < 0) {
-    return res.status(400).send("The message with id provided was not deleted");
-  }
-  messages.splice(i, 1);
-  save();
-  res.status(200).send(messages);
+  const id = parseInt(req.params.ts);
+  conn.then((client) => {
+    client
+      .db("messages")
+      .collection("message")
+      .deleteOne({ts:id})
+      .then((data) => {
+        res.status(200).send(data);
+        console.log(data);
+      });
+  }).catch((err)=>{
+    res.status(400).send(err);
+  });
+
+  //res.status(200).send(messages);
   //Update
   ws.send("");
 };
 
 /**
  * Method that updates a message with the given information.
- * @param {req} req 
- * @param {res} res 
- * @param {next} next 
+ * @param {req} req
+ * @param {res} res
+ * @param {next} next
  */
 exports.updateMessage = (req, res, next) => {
-  let i = messages.findIndex(
-    (item) => item.ts === parseInt(req.body.ts)
-  );
   const { error } = messageValidation.validate(req.body);
-  if (i < 0){
-    return res.status(400).send("The message with id provided was not updated.");
-  }
-  if (error){
+  if (error) {
     return res.status(400).send(error);
   }
-  messages[i] = makeMessage(req.body);
-  save();
-  res.status(200).send(messages);
+  const id = parseInt(req.body.ts);
+  console.log(id);
+  conn.then((client) => {
+    const newMessage = makeMessage(req.body);
+    newMessage.ts = id;
+    client
+      .db("messages")
+      .collection("message")
+      .updateOne({ts:id},{$set: {message:newMessage.message,author:newMessage.author}})
+      .then((data) => {
+        res.status(200).send(newMessage);
+        console.log(data);
+      });
+  }).catch((err)=>{
+    res.status(400).send(err);
+  });
+
+  //res.status(200).send(messages);
   //Update
   ws.send("");
 };
